ae0292fa7da9f0b21b15f04ab6fa6e9bd0f85257
Claus Kaiser <c.kaiser.p@gmail.com>
2017-01-06 03:20:40 +0100

    addition of MobileDetectModule

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
 			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 			new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 			new \Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle(),
+			new SunCat\MobileDetectBundle\MobileDetectBundle(),
 		];
 
 		if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
