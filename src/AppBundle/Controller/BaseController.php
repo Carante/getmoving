876a2e17b79f96b6a4d94b46cc45e778420caaef
Claus Kaiser <c.kaiser.p@gmail.com>
2017-01-15 20:54:40 +0100

    noreply email address changed

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -190,7 +190,7 @@ abstract class BaseController extends Controller
 		$message = \Swift_Message::newInstance($name." | Welcome")
 			->setFrom(array($supportEmail => "NoReply | ".$name))
 			->setTo(array($userEmail => $fullName))
-			->setReplyTo(array($noReplyEmail => "NoReply"))
+			->setReplyTo(array($supportEmail => "NoReply"))
 			->setBody($body, 'text/html');
 
 		return $mailer->send($message);
